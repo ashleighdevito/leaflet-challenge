e398e3294eff2ac7ad90de1a9892d1d410012df6
ashleigh <ashleighdevito@gmail.com>
2021-08-23 21:57:22 -0400

    Step 1

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,20 +1,27 @@
+//define map
 var myMap = L.map("map", {
 center: [35, 66],
 zoom: 4,
 });
 
+//define baselayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(myMap);
 
-  // Store our API endpoint as queryUrl.
+//define query
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+//set color gradient
 var colors = ["#feb24c", "#fc4e2a", "#bd0026", "#49006a"];
 
+//function to create markers for each data point
 function createFeatures(earthquakeData) {
 
+  //function to set the markersize correlated to quake magnitude
   function markerSize(magnitude) {
     return Math.exp(magnitude)*1000;}
+
+  //function to set the markercolor correlated to quake depth 
   function markerColor(depth) {
     if (depth < 20) return colors[0];
     else if (depth >= 20 && depth < 40) return colors[1];
@@ -22,6 +29,7 @@ function createFeatures(earthquakeData) {
     else if (depth >= 60) return colors[3];
   }
 
+  //function to define attributes for each marker
   function onEachFeature(feature, marker) {
     
     quakemag = feature.properties.mag
@@ -34,29 +42,31 @@ function createFeatures(earthquakeData) {
     marker.bindPopup(`<h3>Magnitude: ${quakemag}<span style="margin-left:40px">Depth: ${quakedepth}km</span></h3><hr><h4>Location: ${feature.properties.place}</h4><hr><p>Time: ${new Date(feature.properties.time)}</p>`).addTo(myMap);
   }
 
+  //create a layer by passing all of the quake data through the feature function
   L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature
   });
 
 };
-// Perform a GET request to the query URL/
+//perform a get request for the URL
 d3.json(queryUrl).then(function (data) {
-    // Once we get a response, send the data.features object to the createFeatures function.
+    //when data aquired, send data abject createFeatures function.
     createFeatures(data.features);
   });
 
-// Set up the legend.
+//set up the legend
 var legend = L.control({ position: "bottomright" });
 legend.onAdd = function() {
   var div = L.DomUtil.create("div", "info legend");
   var limits = ["Less than 20km", "From 20 to 40km", "From 40 to 60km", "60km and Greater"];
   var labels = [];
 
-  // Add the minimum and maximum.
+  //add title
   var legendInfo = "<h1>Earthquake Depth</h1>";
 
   div.innerHTML = legendInfo;
 
+  //create HTML for each depth possibility
   limits.forEach(function(limit, index) {
     labels.push("<li style=\"background-color: " + colors[index] + ";padding-left: 10px\">" + limit + "</li>");
   });
@@ -65,6 +75,6 @@ legend.onAdd = function() {
   return div;
 };
 
-// Adding the legend to the map
+//add the legend to the map
 legend.addTo(myMap);
   
